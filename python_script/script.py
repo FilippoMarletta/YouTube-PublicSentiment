d2da5f7f98e0ba4587baf6b62a3859025e584224
Filippo Marletta <filippomarletta9@gmail.com>
2024-07-16 23:20:00 +0200

    refactor(youtubecomments service): now the input link is passed as environment variable
    also rewrote comments in english

--- python_script/script.py
+++ python_script/script.py
@@ -3,51 +3,52 @@ import time
 import requests
 import html
 import re
+import os
 from googleapiclient.discovery import build
 from urllib.parse import urlparse, parse_qs
 from get_docker_secret import get_docker_secret
 
 
-# Configura le credenziali delle API
+# configuration variables for YouTube API
 API_KEY = get_docker_secret('youtube_api_key')
 PARTS = 'snippet,replies'
-ORDER = 'time'  # Ordina i commenti per data di creazione dal più recente all'Meno recente
+ORDER = 'time'  # Order comments by creation date from most recent to least recent
 MAX_RESULTS = 30
 
 
-# URL dell'istanza di Logstash
+# URL of Logstash instance
 LOGSTASH_URL = 'http://logstash-container:9090'
 
-# Inizializza il servizio delle API di YouTube
+# Initialize YouTube API service
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
-# Deque per tenere traccia degli ID dei commenti già letti
+# Deque to keep track of processed comment IDs
 processed_comment_ids = set()
 
 
 def remove_html_tags(text):
-    """Remove html tags from a string"""
+    """Remove HTML tags from a string"""
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
 
 def get_video_id(url):
     try:
-        # Estrai l'ID del video dall'URL
+        # Extract video ID from URL
         query = urlparse(url).query
         params = parse_qs(query)
         video_id = params.get('v')
         if video_id:
             return video_id[0]
         else:
-            raise ValueError('URL del video non valido')
+            raise ValueError('Invalid video URL')
     except Exception as e:
-        print(f"Errore nell'estrazione dell'ID del video: {e}")
+        print(f"Error extracting video ID: {e}")
         sys.exit(1)
 
 
 def get_comments(video_id, page_token=None):
     try:
-        # Richiedi i commenti del video
+        # Request video comments
         request = youtube.commentThreads().list(
             part=PARTS,
             videoId=video_id,
@@ -58,17 +59,17 @@ def get_comments(video_id, page_token=None):
         response = request.execute()
         return response
     except Exception as e:
-        print(f"Errore nella richiesta dei commenti: {e}")
+        print(f"Error requesting comments: {e}")
         return None
 
 
 def send_to_logstash(comment):
     try:
-        # Invia i commenti a Logstash
+        # Send comments to Logstash
         response = requests.post(LOGSTASH_URL, json=comment)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        print(f"Errore nell'invio a Logstash: {e}")
+        print(f"Error sending to Logstash: {e}")
         
 def process_comment(comment):
     global comment_count
@@ -94,29 +95,29 @@ def process_comment(comment):
           
         print(f"{id} - {published_at} - {author}: {text}")
 
-        # Invia il commento a Logstash
+        # Send the comment to Logstash
         send_to_logstash(comment_data)
 
-        # Aggiungi l'ID del commento alla deque
+        # Add the comment ID to the deque
         processed_comment_ids.add(id)
         comment_count += 1
         return True
     except KeyError as e:
-        print(f"Errore nel processare un commento: {e}")
+        print(f"Error processing a comment: {e}")
         return False
 
 if __name__ == '__main__':
     try:
-        # Controllo argomenti
-        if len(sys.argv) < 2:
-            print("Usage: python script.py <url>")
+        # Get video URL from environment variable
+        video_url = os.getenv('VIDEO_URL')
+        if not video_url:
+            print("Error: VIDEO_URL environment variable not set")
             sys.exit(1)
-        
-        video_url = sys.argv[1]
-        print(f"URL del video: {video_url}")
+            
+        print(f"Video URL: {video_url}")
         
         video_id = get_video_id(video_url)
-        print(f"ID del video: {video_id}")
+        print(f"Video ID: {video_id}")
         
         global comment_count
         comment_count = 0
@@ -125,41 +126,40 @@ if __name__ == '__main__':
         while True:
             response = get_comments(video_id, page_token)
             if response is None:
-                print("Errore nel recupero dei commenti, riprovo dopo 10 minuti...")
+                print("Error retrieving comments, retrying after 10 minutes...")
                 time.sleep(600)
                 continue
 
             comments = response.get('items', [])
             
             if not comments:
-                print("Nessun nuovo commento trovato. Attendo 35 secondi...")
+                print("No new comment found. Waiting for 30 seconds...")
                 time.sleep(30)
                 page_token = None
                 continue
 
-         
-
             new_comments = []
-            for comment in comments:  # Invertiamo l'ordine per processare dal più vecchio al più recente
+            for comment in comments[::-1]:  # Process comments from oldest to newest
                 if comment['id'] not in processed_comment_ids:
                     new_comments.append(comment)
                 else:
-                    break  # Abbiamo raggiunto un commento già processato
+                    break  # Reached a processed comment
 
             for comment in new_comments:
                 process_comment(comment)
                 
-            print(f"Numero totale di commenti ritirati fino ad ora: {comment_count}")
+            print(f"Total number of comments retrieved so far: {comment_count}")
 
-            if len(new_comments) == len(comments):  # Tutti i commenti erano nuovi
+            if len(new_comments) == len(comments):  # All comments were new
                 page_token = response.get('nextPageToken')
             else:
-                page_token = None  # Ricomincia dall'inizio per cercare nuovi commenti
+                page_token = None  # Start from the beginning to search for new comments
             
-            time.sleep(30)  # Breve pausa per non sovraccaricare l'API
+            time.sleep(30)  # Short pause to avoid overloading the API
     except Exception as e:
-        print(f"Errore critico: {e}")
+        print(f"Critical error: {e}")
         sys.exit(1)
     except KeyboardInterrupt:
-        print("Script interrotto dall'utente")
+        print("Script interrupted by user")
         sys.exit(0)
+
